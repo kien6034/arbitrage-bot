0fe7eb509d35a3fd0113f1ec8453c8a89bf81c5f
kien6034 <trinhtrungkientb99@gmail.com>
2023-09-25 15:42:38 +0700

    feat: remove random packages

--- node-server/server.ts
+++ node-server/server.ts
@@ -1,29 +1,20 @@
 import path from "path";
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
-import { ProtoGrpcType } from "./proto/random";
-import { RandomHandlers } from "./proto/randomPackage/Random";
 import { ProtoGrpcType as WhirlpoolProtoGrpcType } from "./proto/whirlpool";
 import { WhirlpoolHandlers } from "./proto/whirlpool/Whirlpool";
 import { Whirlpool } from "./services/whirlpool/whirlpool";
 
 const PORT = 8082;
-const PROTO_FILE = "./proto/random.proto";
 const WHIRLPOOL_PROTO_FILE = "./proto/whirlpool.proto";
 
-const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
 const whirlpoolPackageDef = protoLoader.loadSync(
   path.resolve(__dirname, WHIRLPOOL_PROTO_FILE)
 );
-const grpcObj = grpc.loadPackageDefinition(
-  packageDef
-) as unknown as ProtoGrpcType;
 
 const whirlpoolGrpcObj = grpc.loadPackageDefinition(
   whirlpoolPackageDef
 ) as unknown as WhirlpoolProtoGrpcType;
-
-const randomPackage = grpcObj.randomPackage;
 const whirlpoolPackage = whirlpoolGrpcObj.whirlpool;
 
 function main() {
@@ -45,12 +36,6 @@ function main() {
 
 function getServer() {
   const server = new grpc.Server();
-  server.addService(randomPackage.Random.service, {
-    PingPong: (req, res) => {
-      console.log(req.request);
-      res(null, { message: "Pong" });
-    },
-  } as RandomHandlers);
 
   server.addService(whirlpoolPackage.Whirlpool.service, {
     GetPrice: (req, res) => {
---
+++ tree.sh
@@ -0,0 +1,1 @@
+tree -I 'node_modules|go.sum|go.mod|.wallets' > output.txt
