56cc127c5157ac8212682b5442122adc55d292bd
kien6034 <trinhtrungkientb99@gmail.com>
2023-12-24 00:47:42 +0700

    feat: hmac and sha1 to connect to remi

---
+++ core/cex/remitano/remitano.go
@@ -0,0 +1,100 @@
+package remitano
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const RemitanoBaseURL = "https://api.remitano.com/api/v1/"
+
+func ComputeHmac(secret, method, data string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(data))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func getStringData(data string) string {
+	if data == "" {
+		// If data is an empty string, treat it as an empty JSON object to match TypeScript behavior
+		data = "{}"
+	}
+	return data
+}
+
+func ComputeMD5(data string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(getStringData(data)))
+	md5Hash := base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+	return md5Hash
+}
+
+type Remitano struct {
+	AccessKey string
+	SecretKey string
+}
+
+func NewRemitano(accessKey, secretKey string) *Remitano {
+	return &Remitano{
+		AccessKey: accessKey,
+		SecretKey: secretKey,
+	}
+}
+
+func (r *Remitano) DoApiRequest(url, method, data string) (*http.Response, error) {
+	if r.SecretKey == "" || r.AccessKey == "" {
+		return nil, fmt.Errorf("missing API keys")
+	}
+
+	client := &http.Client{}
+	fullURL := RemitanoBaseURL + url
+	req, err := http.NewRequest(method, fullURL, strings.NewReader(getStringData(data)))
+	if err != nil {
+		return nil, err
+	}
+
+	md5Hash := ComputeMD5(data)
+	date := time.Now().UTC()
+	formattedDate := strings.ReplaceAll(date.Format(time.RFC1123), "UTC", "GMT")
+
+	reqUrl := "/api/v1/" + url
+	signatureString := fmt.Sprintf("%s,application/json,%s,%s,%s", method, md5Hash, reqUrl, formattedDate)
+	hmacSha1 := ComputeHmacSHA1(r.SecretKey, signatureString)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-MD5", md5Hash)
+	req.Header.Set("Date", formattedDate)
+	req.Header.Set("Authorization", fmt.Sprintf("APIAuth %s:%s", r.AccessKey, hmacSha1))
+
+	return client.Do(req)
+}
+
+func ComputeHmacSHA1(secret, data string) string {
+	key := []byte(secret)
+	h := hmac.New(sha1.New, key)
+	h.Write([]byte(data))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func (r *Remitano) GetBalance(tokens []string) error {
+	response, err := r.DoApiRequest("users/coin_accounts", "GET", "")
+	if err != nil {
+		fmt.Println("erro when do request")
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("API request failed with status code: %d", response.StatusCode)
+	}
+
+	fmt.Println(response)
+	return nil
+}
--- core/main.go
+++ core/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"os"
 	"time"
 
 	whirlpool "github.com/kien6034/arbitrage-bot/core/proto/whirlpool"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	remitano "github.com/kien6034/arbitrage-bot/core/cex/remitano"
 )
 
 const (
@@ -22,39 +21,14 @@ var (
 )
 
 func main() {
-	flag.Parse()
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	accessKey := os.Getenv("ACCESS_KEY")
+	secretKey := os.Getenv("SECRET_KEY")
+
+	rm := remitano.NewRemitano(accessKey, secretKey)
+	err := rm.GetBalance([]string{"renec", "usdt"})
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		panic(err)
 	}
-	defer conn.Close()
-	c := whirlpool.NewWhirlpoolClient(conn)
-
-	// Start timing
-	startTime := time.Now()
-
-	respChan := make(chan *whirlpool.GetPriceResponse)
-	errChan := make(chan error)
-
-	// Start two concurrent requests
-	go makeRequest(c, "Afy8qEgeJykFziRwiCk6tnBbd3uzxMoEqn2GTNCyGN7P", "So11111111111111111111111111111111111111112", respChan, errChan)
-	go makeRequest(c, "Afy8qEgeJykFziRwiCk6tnBbd3uzxMoEqn2GTNCyGN7P", "So11111111111111111111111111111111111111112", respChan, errChan)
-
-	// Wait for responses
-	for i := 0; i < 2; i++ {
-		select {
-		case resp := <-respChan:
-			log.Printf("Data: %f", resp.Price)
-		case err := <-errChan:
-			log.Printf("Error: %v", err)
-		}
-	}
-
-	// Calculate elapsed time
-	elapsedTime := time.Since(startTime)
-
-	log.Printf("Request took: %s", elapsedTime)
 }
 
 func makeRequest(client whirlpool.WhirlpoolClient, tokenA, tokenB string, respChan chan<- *whirlpool.GetPriceResponse, errChan chan<- error) {
